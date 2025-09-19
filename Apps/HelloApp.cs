869598f9944091510245d7da14cbfb7faafd841f
Sergey Karpov <cliovox0409@gmail.com>
2025-09-19 16:20:47 -0400

    correct folder

--- Apps/HelloApp.cs
+++ Apps/HelloApp.cs
@@ -5,7 +5,7 @@ public class HelloApp : ViewBase
 {
     public override object? Build()
     {
-        var nameState = this.UseState<string>();
+        var nameState = this.UseState<string>("Sergey");
 
         return Layout.Center()
                | (new Card(
@@ -16,7 +16,13 @@ public class HelloApp : ViewBase
                    | nameState.ToInput(placeholder: "What is your name?")
                    | new Separator()
                    | Text.Markdown("You'd be a hero to us if you could ⭐ us on [Github](https://github.com/Ivy-Interactive/Ivy-Framework)")
+                   | Text.H2("Hello Niels")
+                   | Text.Block("I'm Sergey Karpov from Westchester, NY")
+                   | Text.Block("Very pleased to see your invitation and hope for a productive work promoting Ivy worldwide!")
+                   | Text.Markdown("I've placed my introductory Ivy application [here at my Github](https://github.com/SergeyKarpov914/Ivy)")
                  )
                  .Width(Size.Units(120).Max(500)));
+
+
     }
 }
